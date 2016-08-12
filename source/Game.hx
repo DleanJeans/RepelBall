31883bfdcf1fe07f53c2c506ee4a6d0868327ec6
Dlean Jeans <dleanjeans@gmail.com>
2016-08-12 17:39:16 +0700

    Game: Add Level, Signals and Collision

--- source/Game.hx
+++ source/Game.hx
@@ -1,5 +1,9 @@
 package;
+
+import systems.Level;
 import systems.Renderer;
+import systems.Signals;
+import systems.collisions.Collision;
 
 class Game {
 	private static inline var UNIT_LENGTH = 16;
@@ -7,9 +11,19 @@ class Game {
 		return cast UNIT_LENGTH * times;
 	}
 	
+	public static var level(default, null):Level;
+	
+	public static var signals(default, null):Signals;
 	public static var renderer(default, null):Renderer;
+	public static var collision(default, null):Collision;
 	
 	public static function init() {
+		signals = new Signals();
 		renderer = new Renderer();
+		collision = new Collision();
+	}
+	
+	public static inline function startNewLevel() {
+		level = new Level();
 	}
 }
