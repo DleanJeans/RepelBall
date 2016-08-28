c30522bf985a03d25d892c70274257caedaee68d
Dlean Jeans <dleanjeans@gmail.com>
2016-08-28 16:18:23 +0700

    Add ColorSwatchSelector

---
+++ source/systems/ui/ColorSwatchSelector.hx
@@ -0,0 +1,115 @@
+package systems.ui;
+
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.mouse.FlxMouseEventManager;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+using flixel.util.FlxSpriteUtil;
+using flixel.addons.util.position.FlxPosition;
+
+class ColorSwatchSelector extends FlxSpriteGroup {
+	public var label(default, null):FlxText;
+	public var swatches(default, null):FlxSpriteGroup;
+	public var selector(default, null):FlxSprite;
+	public var maxColumns:Int;
+	public var swatchWidth:Int;
+	public var swatchHeight:Int;
+	public var spacingX:Int;
+	public var spacingY:Int;
+	
+	private var selectorTween:FlxTween;
+	private var selectedSwatch:FlxSprite;
+
+	public function new(colors:Array<FlxColor>, x:Float = 0, y:Float = 0, swatchWidth:Int = 30, swatchHeight:Int = 30, maxColumns:Int = 4, spacingX:Int = 2, spacingY:Int = 2) {
+		super();
+		
+		setPosition(x, y);
+		
+		this.maxColumns = maxColumns;
+		this.swatchWidth = swatchWidth;
+		this.swatchHeight = swatchHeight;
+		this.spacingX = spacingX;
+		this.spacingY = spacingY;
+		
+		label = new FlxText(0, 0, (swatchWidth + spacingX) * maxColumns, "Color", 20);
+		label.alignment = FlxTextAlign.CENTER;
+		add(label);
+		
+		swatches = new FlxSpriteGroup();
+		add(swatches);
+		
+		selector = new FlxSprite();
+		selector.makeGraphic(swatchWidth + 2, swatchHeight + 2, 0x0);
+		selector.drawRect(0, 0, selector.width, selector.height, 0x0, { thickness:6, color:Game.colors.white });
+		selector.setSize(0, 0);
+		selector.centerOffsets();
+		add(selector);
+		
+		var swatch:FlxSprite;
+		for (color in colors) {
+			swatch = new FlxSprite();
+			swatch.makeGraphic(swatchWidth, swatchHeight);
+			swatch.color = color;
+			swatches.add(swatch);
+			FlxMouseEventManager.add(swatch, selectSwatch);
+		}
+		
+		updateLayout();
+		selectSwatch(getFirstSwatch());
+	}
+	
+	private inline function getFirstSwatch() {
+		return swatches.members[0];
+	}
+	
+	private inline function selectSwatch(swatch:FlxSprite) {
+		moveSelectorTo(swatch);
+		selectedSwatch = swatch;
+	}
+	
+	private function moveSelectorTo(swatch:FlxSprite) {
+		if (selectorTween != null && !selectorTween.finished)
+			selectorTween.cancel();
+		selectorTween = FlxTween.tween(selector, { x:swatch.getCenterX(), y:swatch.getCenterY()}, 0.25, { ease:FlxEase.quartOut});
+	}
+	
+	private function updateLayout() {
+		var column = 0;
+		var row = 0;
+		
+		for (swatch in swatches) {
+			var x = swatches.x + (swatchWidth + spacingX) * column;
+			var y = swatches.y + (swatchHeight + spacingY) * row;
+			swatch.setPosition(x, y);
+			
+			column++;
+			if (column >= maxColumns) {
+				column = 0;
+				row++;
+			}
+		}
+		
+		swatches.y = label.getBottom();
+		selector.setPosition(getFirstSwatch().getCenterX(), getFirstSwatch().getCenterY());
+	}
+	
+	public inline function getColor() {
+		return selectedSwatch.color;
+	}
+	
+	public inline function getColorName() {
+		return Game.colors.getName(getColor());
+	}
+	
+	override public function destroy():Void {
+		for (swatch in swatches) {
+			FlxMouseEventManager.remove(swatch);
+		}
+		super.destroy();
+	}
+	
+}
