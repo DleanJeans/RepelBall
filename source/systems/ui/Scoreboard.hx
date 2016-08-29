8f91fb828d697559389c235bbe7d1e9ebfc5bf95
Dlean Jeans <dleanjeans@gmail.com>
2016-08-29 22:32:28 +0700

    Scoreboard: Reposition
    + Fix crash with graphic.persist = true

--- source/systems/ui/Scoreboard.hx
+++ source/systems/ui/Scoreboard.hx
@@ -3,7 +3,6 @@ package systems.ui;
 import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
-import flixel.util.FlxAxes;
 import flixel.util.FlxColor;
 import objects.Ball;
 import objects.Wall;
@@ -26,6 +25,11 @@ class Scoreboard extends FlxSpriteGroup {
 		moreSetup();
 	}
 	
+	public function updateColors() {
+		background1.color = Game.match.team1.color;
+		background2.color = Game.match.team2.color;
+	}
+	
 	public function updateScores() {
 		score1.text = Std.string(Game.match.team1.score);
 		score2.text = Std.string(Game.match.team2.score);
@@ -46,9 +50,16 @@ class Scoreboard extends FlxSpriteGroup {
 	}
 	
 	private function setupStuff() {
-		background1.makeGraphic(cast score1.width * 1.25, cast score1.height * 1.25);
-		background2.makeGraphic(cast score2.width * 1.25, cast score2.height * 1.25);
-		background2.setTopLeft(background1.getTopRight());
+		background1.alpha = background2.alpha = 0.75;
+		background1.makeGraphic(cast score1.width * 1.5, cast score1.height * 1.5);
+		background2.makeGraphic(cast score2.width * 1.5, cast score2.height * 1.5);
+		background1.setTopLeft(background2.getBottomLeft());
+		background1.graphic.persist = true;
+		background2.graphic.persist = true;
+		
+		score1.fieldWidth = background1.width;
+		score2.fieldWidth = background2.height;
+		score1.alignment = score2.alignment = FlxTextAlign.CENTER;
 	}
 	
 	private function addStuff() {
@@ -59,10 +70,7 @@ class Scoreboard extends FlxSpriteGroup {
 	}
 	
 	private function moreSetup() {
-		screenCenter(FlxAxes.X);
-		
-		background1.color = Game.color.green;
-		background2.color = Game.color.blue;
+		setMidLeft(FlxPosition.screenMidLeft);
 	}
 	
 }
