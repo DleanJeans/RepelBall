c393335ee13465bc7eef040656c32e5ad4fd4cf3
Dlean Jeans <dleanjeans@gmail.com>
2016-08-29 08:47:44 +0700

    TeamSettings: Add Paddle

--- source/systems/ui/TeamSettings.hx
+++ source/systems/ui/TeamSettings.hx
@@ -1,12 +1,20 @@
 package systems.ui;
 
+import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import objects.Paddle;
 using flixel.addons.util.position.FlxPosition;
 
 class TeamSettings extends FlxSpriteGroup {
 	public var teamName:FlxText;
 	public var colorSwatch:ColorSwatchSelector;
+	
+	public var paddleBack:FlxSprite;
+	public var paddle:Paddle;
+	
+	public var controlSettingsButton:FlxButton;
 	
 	public function new(x:Float = 0, y:Float = 0) {
 		super();
@@ -15,6 +23,9 @@ class TeamSettings extends FlxSpriteGroup {
 		
 		teamName = new FlxText();
 		colorSwatch = Game.pools.getDefaultColorSwatch();
+		paddleBack = new FlxSprite();
+		paddle = Game.pools.getPaddle();
+		controlSettingsButton = new FlxButton();
 		
 		teamName.size = 30;
 		teamName.fieldWidth = colorSwatch.width * 1.75;
@@ -25,8 +36,19 @@ class TeamSettings extends FlxSpriteGroup {
 		colorSwatch.colorChangedCallback = updateTeamName;
 		colorSwatch.y += 20;
 		
+		paddleBack.makeGraphic(150, 150);
+		paddleBack.alpha = 0.25;
+		paddleBack.setMidTop(colorSwatch.getMidBottom());
+		paddleBack.y += 30;
+		
+		paddle.scale.set(1.5, 1.5);
+		paddle.updateHitbox();
+		paddle.setCenter(paddleBack.getCenter());
+		
 		add(teamName);
 		add(colorSwatch);
+		add(paddleBack);
+		add(paddle);
 		
 		updateTeamName(colorSwatch);
 		colorSwatch.fixSelector();
@@ -35,6 +57,7 @@ class TeamSettings extends FlxSpriteGroup {
 	private function updateTeamName(colorSwatch:ColorSwatchSelector) {
 		teamName.text = "Team " + colorSwatch.getColorName();
 		teamName.color = colorSwatch.getColor();
+		paddle.color = colorSwatch.getColor();
 	}
 	
 }
