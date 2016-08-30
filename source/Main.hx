3d8b74dbf8ebe066c3d9993c85f3f18d75644bd1
Dlean Jeans <dleanjeans@gmail.com>
2016-08-30 09:45:19 +0700

    Main: Different drawFrame on debug/release

--- source/Main.hx
+++ source/Main.hx
@@ -10,9 +10,14 @@ class Main extends Sprite
 {
 	public function new()
 	{
+		var drawFramerate =
+		#if debug 30
+		#else 60
+		#end;
+		
 		super();
 		// init Game before first state create()
 		FlxG.signals.preStateCreate.addOnce(function(state:FlxState) Game.init());
-		addChild(new FlxGame(600, 800, MenuState, 1, 60, 30, true));
+		addChild(new FlxGame(600, 800, MenuState, 1, 60, drawFramerate, true));
 	}
 }
