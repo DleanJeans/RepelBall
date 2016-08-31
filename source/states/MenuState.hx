2292cc4a20ba0ca07fada205a70bb6ec7e0817f4
Dlean Jeans <dleanjeans@gmail.com>
2016-08-31 12:02:34 +0700

    Add MenuState and PlayState

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -2,13 +2,48 @@ package states;
 
 import flixel.FlxG;
 import flixel.FlxState;
-
+import states.group.MatchSettings;
+import states.group.TitleMenu;
+	
 class MenuState extends FlxState {
+	public var titleMenu:TitleMenu;
+	public var matchSettings:MatchSettings;
+	
 	override public function create() {
+		bgColor = 0xFF212121;
+		camera.antialiasing = true;
 		
+		titleMenu = new TitleMenu();
+		matchSettings = new MatchSettings();
+		
+		titleMenu.newMatch.add(switchToMatchSettings);
+		matchSettings.back.add(switchToTitleMenu);
+		matchSettings.start.add(startMatch);
+		
+		add(titleMenu);
+		add(matchSettings);
+		
+		switchToTitleMenu();
 	}
-
-	override public function update(elapsed:Float) {
-		super.update(elapsed);
+	
+	private function switchToMatchSettings() {
+		titleMenu.visible = false;
+		matchSettings.visible = true;
+	}
+	
+	private function switchToTitleMenu() {
+		titleMenu.visible = true;
+		matchSettings.visible = false;
+	}
+	
+	private function startMatch() {
+		if (matchSettings.sameTeamColor()) {
+			Game.states.warning();
+			return;
+		}
+		
+		matchSettings.apply();
+		Game.states.play();
 	}
 }
+
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,14 +1,46 @@
 package states;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.system.debug.FlxDebugger.FlxDebuggerLayout;
+import flixel.system.debug.watch.Tracker.TrackerProfile;
+import flixel.util.FlxAxes;
+import objects.Ball;
+import objects.Paddle;
+import objects.Wall;
+import systems.Level;
+import systems.match.Team;
+import systems.ui.Scoreboard;
+using flixel.addons.util.position.FlxPosition;
 
 class PlayState extends FlxState {
+	var paddle:Paddle;
+	var paddle2:Paddle;
+	
 	override public function create() {
+		bgColor = Game.color.gray;
 		
+		add(Game.level);
+		
+		paddle = Game.match.team1.paddles[0];
+		Game.level.addPaddle(paddle);
+		
+		Game.controllers.addNewPlayerController(paddle);
+		
+		paddle2 = Game.match.team2.paddles[0];
+		Game.level.addPaddle(paddle2);
+		Game.controllers.addNewSimpleAI(paddle2, Game.walls.topWall);
+		
+		Game.match.team1.setGoal(Game.walls.bottomWall);
+		Game.match.team2.setGoal(Game.walls.topWall);
+		
+		Game.signals.matchStart.dispatch();
+		Game.signals.preRoundStart.dispatch();
 	}
-
-	override public function update(elapsed:Float) {
-		super.update(elapsed);
+	
+	override public function destroy():Void {
+		remove(Game.level);
+		super.destroy();
 	}
 }
