1385b8c7e8d7f64c53172f77ec296246d256fd1e
Dlean Jeans <dleanjeans@gmail.com>
2016-09-01 16:17:17 +0700

    Colors: Make getters private

--- source/systems/Colors.hx
+++ source/systems/Colors.hx
@@ -25,19 +25,19 @@ class Colors {
 		return list;
 	}
 	
-	public inline function get_red():FlxColor return 0xFFF44336;
-	public inline function get_pink():FlxColor return 0xFFFF4081;
-	public inline function get_purple():FlxColor return 0xFF9C27B0;
-	public inline function get_blue():FlxColor return 0xFF2196F3;
-	public inline function get_green():FlxColor return 0xFF00E676;
-	public inline function get_lime():FlxColor return 0xFFAEEA00;
-	public inline function get_yellow():FlxColor return 0xFFFFEB3B;
-	public inline function get_orange():FlxColor return 0xFFFF9800;
+	inline function get_red():FlxColor return 0xFFF44336;
+	inline function get_pink():FlxColor return 0xFFFF4081;
+	inline function get_purple():FlxColor return 0xFF9C27B0;
+	inline function get_blue():FlxColor return 0xFF2196F3;
+	inline function get_green():FlxColor return 0xFF00E676;
+	inline function get_lime():FlxColor return 0xFFAEEA00;
+	inline function get_yellow():FlxColor return 0xFFFFEB3B;
+	inline function get_orange():FlxColor return 0xFFFF9800;
 	
-	public inline function get_black():FlxColor return 0xFF000000;
-	public inline function get_white():FlxColor return 0xFFFFFFFF;
-	public inline function get_gray():FlxColor return 0xFF212121;
-	public inline function get_transWhite():FlxColor {
+	inline function get_black():FlxColor return 0xFF000000;
+	inline function get_white():FlxColor return 0xFFFFFFFF;
+	inline function get_gray():FlxColor return 0xFF212121;
+	inline function get_transWhite():FlxColor {
 		var white = FlxColor.WHITE;
 		white.alphaFloat = 0.25;
 		return white;
