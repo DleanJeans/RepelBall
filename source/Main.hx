848c1805cfc2f07e5062e3f69fa677de335c0575
Dlean Jeans <dleanjeans@gmail.com>
2016-09-01 10:17:13 +0700

    Main: Resolutions for different targets

--- source/Main.hx
+++ source/Main.hx
@@ -3,21 +3,41 @@ package;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
+import openfl.Lib;
 import openfl.display.Sprite;
 import states.MenuState;
 
-class Main extends Sprite
-{
-	public function new()
-	{
-		var drawFramerate =
-		#if debug 30
-		#else 60
-		#end;
-		
+class Main extends Sprite {
+	public var gameWidth(get, never):Int;
+	public var gameHeight(get, never):Int;
+	public var drawFramerate(get, never):Int;
+	
+	public function new() {
 		super();
 		// init Game before first state create()
 		FlxG.signals.preStateCreate.addOnce(function(state:FlxState) Game.init());
-		addChild(new FlxGame(600, 800, MenuState, 1, 60, drawFramerate, true));
+		addChild(new FlxGame(gameWidth, gameHeight, MenuState, 1, 60, drawFramerate, true));
 	}
+	
+	inline function get_gameWidth():Int {
+		return 600;
+	}
+	
+	inline function get_gameHeight():Int {
+		return
+		#if mobile
+		cast stage.stageHeight / stage.stageWidth * gameWidth
+		#else
+		800
+		#end;
+	}
+	
+	inline function get_drawFramerate():Int {
+		return
+		#if debug 30
+		#else 60
+		#end;
+	}
+	
+	
 }
