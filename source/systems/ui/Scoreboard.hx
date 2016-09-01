bcef8da06a6249cd23edcad5a7dea808d997c076
Dlean Jeans <dleanjeans@gmail.com>
2016-09-01 10:18:46 +0700

    Scoreboard: Refactor

--- source/systems/ui/Scoreboard.hx
+++ source/systems/ui/Scoreboard.hx
@@ -10,11 +10,11 @@ import objects.Wall;
 using flixel.addons.util.position.FlxPosition;
 
 class Scoreboard extends FlxSpriteGroup {
-	public var background1:FlxSprite;
-	public var background2:FlxSprite;
+	public var background1(default, null):FlxSprite;
+	public var background2(default, null):FlxSprite;
 	
-	public var score1:FlxText;
-	public var score2:FlxText;
+	public var score1(default, null):FlxText;
+	public var score2(default, null):FlxText;
 
 	public function new() {
 		super();
@@ -34,13 +34,6 @@ class Scoreboard extends FlxSpriteGroup {
 	public function updateScores() {
 		score1.text = Std.string(Game.match.team1.score);
 		score2.text = Std.string(Game.match.team2.score);
-		
-		updateTextPosition();
-	}
-	
-	private function updateTextPosition() {
-		score1.setCenter(background1.getCenter());
-		score2.setCenter(background2.getCenter());
 	}
 	
 	private function createStuff() {
@@ -51,16 +44,30 @@ class Scoreboard extends FlxSpriteGroup {
 	}
 	
 	private function setupStuff() {
+		setupBackgrounds();
+		setupTexts();
+		updateTextPosition();
+	}
+	
+	private function setupBackgrounds() {
 		background1.alpha = background2.alpha = 0.75;
 		background1.makeGraphic(cast score1.width * 1.5, cast score1.height * 1.5);
 		background2.makeGraphic(cast score2.width * 1.5, cast score2.height * 1.5);
 		background1.setTopLeft(background2.getBottomLeft());
-		background1.graphic.persist = true;
+		background1.graphic.persist =
 		background2.graphic.persist = true;
-		
+	}
+	
+	private function setupTexts() {
 		score1.fieldWidth = background1.width;
 		score2.fieldWidth = background2.height;
-		score1.alignment = score2.alignment = FlxTextAlign.CENTER;
+		score1.alignment =
+		score2.alignment = FlxTextAlign.CENTER;
+	}
+	
+	private function updateTextPosition() {
+		score1.setCenter(background1.getCenter());
+		score2.setCenter(background2.getCenter());
 	}
 	
 	private function addStuff() {
