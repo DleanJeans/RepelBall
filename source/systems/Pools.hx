635241077fc63d13d7db4620240ee41012ae83b5
Dlean Jeans <dleanjeans@gmail.com>
2016-09-02 09:15:51 +0700

    Revert "Pools Remove Paddle and Wall pools"

--- source/systems/Pools.hx
+++ source/systems/Pools.hx
@@ -12,12 +12,18 @@ using flixel.addons.util.position.FlxPosition;
 
 typedef Pool<T:FlxBasic> = FlxTypedGroup<T>;
 typedef BallPool = Pool<Ball>;
+typedef PaddlePool = Pool<Paddle>;
+typedef WallPool = Pool<Wall>;
 
 class Pools {
 	public var balls(default, null):BallPool;
+	public var paddles(default, null):PaddlePool;
+	public var walls(default, null):WallPool;
 	
 	public function new() {
 		balls = new BallPool();
+		paddles = new PaddlePool();
+		walls = new WallPool();
 	}
 	
 	public function getBall(?position:FlxPoint, ?velocity:FlxPoint):Ball {
@@ -30,13 +36,13 @@ class Pools {
 	}
 	
 	public function getPaddle(?speed:Int):Paddle {
-		var paddle = new Paddle();
+		var paddle = paddles.recycle(Paddle);
 		paddle.speed = speed == null ? FlxG.width : speed;
 		return paddle;
 	}
 	
 	public function getWall(x:Float, y:Float, width:Int, height:Int, facing:Int):Wall {
-		var wall = new Wall();
+		var wall = walls.recycle(Wall);
 		wall.resetWall(x, y, width, height, facing);
 		return wall;
 	}
