f0e60fba51625aa3a4dbf5f8839e8c9b96b04bdc
Dlean Jeans <dleanjeans@gmail.com>
2016-09-13 17:40:31 +0700

    Improve to android-build-reinstall-and-run.bat

--- android-build-reinstall-and-run.bat
+++ android-build-reinstall-and-run.bat
@@ -1,3 +1,4 @@
+lime build android -release
 adb install -r export/android/bin/bin/RepelBall-release.apk
 adb shell am start -n com.bluebirdaward.repelball/com.bluebirdaward.repelball.MainActivity
 pause
