a4ece92928eff1419d25345ebeffe2ac67dc7f16
Dlean Jeans <dleanjeans@gmail.com>
2016-10-09 16:36:58 +0700

    TestState: Add svg test

--- source/test/TestState.hx
+++ source/test/TestState.hx
@@ -1,13 +1,22 @@
 package test;
 
+import flash.display.BitmapData;
+import flash.display.Sprite;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
+import format.SVG;
+import openfl.Assets;
 
 class TestState extends FlxSubState {
-
+	var svg:String = AssetPaths.Icon__svg;
+	
 	override public function create() {
+		var sprite = new FlxSprite();
+		sprite.loadGraphic(Game.renderer.getSvg(svg));
+		sprite.screenCenter();
 		
+		add(sprite);
 	}
 	
 	override public function update(elapsed:Float) {
