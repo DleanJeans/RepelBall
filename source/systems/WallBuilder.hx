0488ea813430b51fb719dd88b1973e93054a4be9
Dlean Jeans <dleanjeans@gmail.com>
2016-10-26 16:52:08 +0700

    WallBuilder: Longer wall

--- source/systems/WallBuilder.hx
+++ source/systems/WallBuilder.hx
@@ -21,16 +21,19 @@ class WallBuilder {
 		var topWallY = 0;
 		var bottomWallY = FlxG.height - wallWidth;
 		
+		// Raise top and bottom walls on mobile
 		if (FlxG.onMobile) {
 			var emptyHeight = FlxG.height * 0.1;
+			#if !js
 			topWallY += cast emptyHeight / 2;
+			#end
 			bottomWallY -= cast emptyHeight / 2;
 		}
 		
 		rightWall = Game.pools.getWall(FlxG.width, 0, wallWidth, FlxG.height, FlxObject.LEFT);
 		leftWall = Game.pools.getWall(-wallWidth, 0, wallWidth, FlxG.height, FlxObject.RIGHT);
-		topWall = Game.pools.getWall(-wallWidth, topWallY, FlxG.width + wallWidth, wallWidth, FlxObject.DOWN);
-		bottomWall = Game.pools.getWall(-wallWidth, bottomWallY, FlxG.width + wallWidth, wallWidth, FlxObject.UP);
+		topWall = Game.pools.getWall(-wallWidth, topWallY, FlxG.width + wallWidth * 2, wallWidth, FlxObject.DOWN);
+		bottomWall = Game.pools.getWall(-wallWidth, bottomWallY, FlxG.width + wallWidth * 2, wallWidth, FlxObject.UP);
 		
 		rightWall.visible = leftWall.visible = false;
 	}
